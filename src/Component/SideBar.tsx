8fae2349efc4b82dd81f659ddf7db2820c80de5e
HanseKim <kimbird2002@naver.com>
2025-02-14 13:17:28 +0900

    2/14 사이드바 크기 수정

--- src/Component/SideBar.tsx
+++ src/Component/SideBar.tsx
@@ -6,7 +6,7 @@ type SideBarProps ={
 }
 const SideBar:React.FC<SideBarProps> = ({isSidebarVisible, toggleSidebar}) =>{
     return (
-        <div className={`transition-transform duration-300 ${isSidebarVisible ? 'translate-x-0' : '-translate-x-full'} bg-white/60 w-64 z-40 h-[50%] fixed left-0 top-0 shadow-lg`}>
+        <div className={`transition-transform duration-300 ${isSidebarVisible ? 'translate-x-0' : '-translate-x-full'} bg-white/60 w-[20%] z-40 h-[50%] fixed left-0 top-0 shadow-lg`}>
             <h2 className="flex flex-row p-4 text-lg font-bold">
                 <div className='w-[60%]'>세하니 사이드 바</div>
                 <button onClick={toggleSidebar}
