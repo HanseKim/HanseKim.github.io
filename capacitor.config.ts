fb6f9a1c9aae5d034a24affb76e136b5de91533c
HanseKim <kimbird2002@naver.com>
2025-09-06 15:28:04 +0900

    Remove Capacitor mobile app setup

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.hansekim.portfolio',
-  appName: 'Portfolio App',
-  webDir: 'build'
-};
-
-export default config;
