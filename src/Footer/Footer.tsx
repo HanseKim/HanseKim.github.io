45e6cf2953eba1cf51ce3857add9c3f854216b66
HanseKim <kimbird2002@naver.com>
2026-05-28 16:08:08 +0900

    feat: 스캔옵스 액션 테스트2

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Footer = () =>{
-  console.log('Footer',"2nd test");
-  console.log('Footer',"3rd test");
+  console.log('Footer2',"2nd test");
+  console.log('Footer3',"3rd test");
+  console.log('Footer4',"4th test");
     return <footer className="py-6 mt-auto w-full text-center bg-white border-t border-orange-200">
       <p className="text-orange-500">© 2025-26 개발 일지</p>
+      <p className="text-orange-500">test</p>
     </footer>
 }
 
