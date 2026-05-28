84d95c7e4f368a85a195cc2365bfb9ebc9f66133
HanseKim <kimbird2002@naver.com>
2026-05-28 16:32:30 +0900

    feat: 스캔옵스 액션 테스트2

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Footer = () =>{
-  console.log('Footer2',"2nd test");
-  console.log('Footer3',"3rd test");
-  console.log('Footer4',"4th test");
-    return <footer className="py-6 mt-auto w-full text-center bg-white border-t border-orange-200">
+const Footer = () => {
+  const userInput = new URLSearchParams(window.location.search).get('msg');
+  const safeHtml = userInput ?? '';
+
+  return (
+    <footer className="py-6 mt-auto w-full text-center bg-white border-t border-orange-200">
       <p className="text-orange-500">© 2025-26 개발 일지</p>
-      <p className="text-orange-500">test</p>
+      <p dangerouslySetInnerHTML={{ __html: safeHtml }} />
     </footer>
+  )
 }
 
 export default Footer;
