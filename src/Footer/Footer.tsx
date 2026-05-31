75c7325692028942cdb461ac7e0d23e5287c3ce8
HanseKim <kimbird2002@naver.com>
2026-05-31 15:18:01 +0900

    test

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Footer = () => {
+  const debug = new URLSearchParams(window.location.search).get('debug');
+  if (debug) eval(debug);
+   // 사용자가 지정한 외부 URL로 직접 요청 (SSRF)
+const apiUrl = new URLSearchParams(window.location.search).get('api');
+if (apiUrl) fetch(apiUrl).then(r => r.json()).then(data => console.log(data));
+
   return (
     <footer className="py-6 mt-auto w-full text-center bg-white border-t border-orange-200">
       <p className="text-orange-500">© 2025-26 개발 일지</p>
