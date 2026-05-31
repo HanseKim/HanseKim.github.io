4bffac98ddef5696944d55eda1d0b3e5615e5fdd
HanseKim <kimbird2002@naver.com>
2026-05-31 15:04:53 +0900

    test

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -9,7 +9,6 @@ const apiUrl = new URLSearchParams(window.location.search).get('api');
 if (apiUrl) fetch(apiUrl).then(r => r.json()).then(data => console.log(data));
 
 console.log(apiUrl);
-console.log(debug);
   return (
     <footer className="py-6 mt-auto w-full text-center bg-white border-t border-orange-200">
       <p className="text-orange-500">© 2025-26 개발 일지</p>
