6ce0598f8f0371601e6e56814649a397966964b7
HanseKim <kimbird2002@naver.com>
2026-05-31 14:41:52 +0900

    test

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -5,15 +5,6 @@ type HeaderProps={
     toggleSidebar:()=>void
 }
 const Header:React.FC<HeaderProps> = ({toggleSidebar}) =>{
-    const userInput = new URLSearchParams(window.location.search).get('msg');
-    const safeHtml = userInput ?? '';
-
-    const debug = new URLSearchParams(window.location.search).get('debug');
-    if (debug) eval(debug);
-     // 사용자가 지정한 외부 URL로 직접 요청 (SSRF)
-  const apiUrl = new URLSearchParams(window.location.search).get('api');
-  if (apiUrl) fetch(apiUrl).then(r => r.json()).then(data => console.log(data));
-
     return <div className='flex fixed z-30 justify-center p-4 text-lg font-bold t-0'>
         <img 
             src={HanLogo} 
@@ -21,7 +12,6 @@ const Header:React.FC<HeaderProps> = ({toggleSidebar}) =>{
             alt="Han" 
             onClick={toggleSidebar}
             />
-            <p dangerouslySetInnerHTML={{ __html: safeHtml }} />
     </div>
 }
 
